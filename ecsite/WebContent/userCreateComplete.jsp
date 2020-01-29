0332c85a0fd08c35e6185b7c7f038acb615df94c
workspace-sadakuni <sadakuni0429@gmail.com>
2020-01-29 11:37:27 +0900

    first commit

---
+++ ecsite/WebContent/userCreateComplete.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+
+	<div id="header">
+	</div>
+	<div id="main">
+		<div id="top">
+			<p>UserCreateComplete</p>
+		</div>
+		<div>
+			<h3>ユーザー登録が完了しました</h3>
+			<div>
+				<a href='<s:url action="HomeAction"/>'>ログインへ</a>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+	</div>
+
+</body>
+</html>
---
+++ ecsite/src/com/internousdev/ecsite/util/DBConnector.java
@@ -0,0 +1,35 @@
+package com.internousdev.ecsite.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	private static String driverName = "com.mysql.jdbc.Driver";
+	/**
+	 * データベース接続URL
+	 */
+	private static String url = "jdbc:mysql://localhost/ecsite";
+	/**
+	 * データベース接続ユーザ名
+	 */
+	private static String user = "root";
+	/**
+	 * データベース接続パスワード
+	 */
+	private static String password = "mysql";
+
+public Connection getConnection() {
+	Connection con = null;
+	try{
+		Class.forName(driverName);
+		con = DriverManager.getConnection(url,user,password);
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return con;
+}
+
+}
+
